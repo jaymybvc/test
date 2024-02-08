9bf464e682ced12503259b3d43d5ed13fa11fd3c
jay659 <jp500861@gmail.com>
2024-02-08 13:29:22 -0700

    Updates

---
+++ js/messageGreeter.js
@@ -0,0 +1,115 @@
+console.log('calksjdkll')
+function display_message(message, time_out, color, duration) {
+	styles = [
+		{
+			background: "hsla(165, 60%, 11%, 1)",
+			background:
+				"linear-gradient(90deg, hsla(165, 60%, 11%, 1) 0%, hsla(149, 54%, 31%, 1) 100%)",
+		},
+		{
+			background: "#8E0E00",
+			background: "linear-gradient(to right, #1F1C18, #8E0E00)",
+		},
+		{
+			background: "#ff7878",
+			background: "linear-gradient(315deg, #ff7878 0%, #ff0000 74%)",
+		},
+		{
+			background: "hsla(189, 86%, 64%, 1)",
+			background:
+				"linear-gradient(90deg, hsla(189, 86%, 64%, 1) 0%, hsla(277, 67%, 77%, 1) 50%, hsla(218, 88%, 59%, 1) 100%)",
+		},
+		{
+			background: "#000000",
+			background: "linear-gradient(147deg, #000000 0%, #04619f 74%)",
+		},
+	];
+
+	setTimeout(() => {
+		Toastify({
+			text: message,
+			duration: duration,
+			destination: "http://127.0.0.1:5501/index.html",
+			newWindow: true,
+			close: false,
+			gravity: "bottom", // `top` or `bottom`
+			position: "right", // `left`, `center` or `right`
+			stopOnFocus: false, // Prevents dismissing of toast on hover
+			style: styles[color],
+			onClick: function () {}, // Callback after click
+		}).showToast();
+	}, time_out);
+}
+
+
+
+$(document).ready(function () {
+	var count = 0;
+	$("#avatar").click(function () {
+		count += 1;
+
+		if (count > 2) {
+			display_message("💀 Please Stop!! That is an image.", 0, 1, 1000);
+		}
+	});
+});
+
+$(document).ready(function () {
+	var count = 0;
+	$("#hamburger").click(function () {
+		count += 1;
+
+		if (count > 2) {
+			display_message("😎 That sounds Dope!!", 1500, 0, 4000);
+		}
+	});
+});
+
+$(document).ready(function () {
+	count = 0;
+	$(".icons ul li a").hover(
+		function () {
+			title = $(this).attr("title");
+			console.log(title);
+			display_message(title, 0, 4, 1000);
+		},
+		() => {}
+	);
+});
+
+COUNT = 0;
+
+$(window).scroll(function () {
+	if ($(window).scrollTop() + window.innerHeight == $(document).height()) {
+		display_message("🙌 Thanks for Visiting.", 0, 0, 2000);
+
+		if (COUNT < 1) {
+			get_location();
+		}
+		COUNT += 1;
+	}
+});
+
+function get_location() {
+	fetch("https://api.ipregistry.co/?key=8imnisuf1vl9yuxv")
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (payload) {
+			display_message(
+				`Hey, Are you from ${payload.location.city}.`,
+				0,
+				0,
+				5000
+			);
+
+			display_message(
+				"👈 Would love to have a converstion with you, let's connect!!",
+				1000,
+				4,
+				5000
+			);
+
+			display_message("🤟 Peace Out", 2000, 0, 5000);
+		});
+}
---
+++ precache-manifest.6c7e6ae2f538361e5818cdb0b429e1aa.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf32a22361f82bd29a7f5e46fb63f174",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "0195b2c29c17082b31f6",
+    "url": "/test/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "1175ec40c783b2e6284d",
+    "url": "/test/static/css/main.0d785890.chunk.css"
+  },
+  {
+    "revision": "0195b2c29c17082b31f6",
+    "url": "/test/static/js/2.fe982a12.chunk.js"
+  },
+  {
+    "revision": "f3b3211170c61884afc00106189a2238",
+    "url": "/test/static/js/2.fe982a12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1175ec40c783b2e6284d",
+    "url": "/test/static/js/main.d496cfd3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/test/static/js/main.d496cfd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542a2592cf37e397fa11",
+    "url": "/test/static/js/runtime-main.2fb3e3c5.js"
+  },
+  {
+    "revision": "20e45d9afe66e0e6533dcd44fb9a7846",
+    "url": "/test/static/media/dev.20e45d9a.svg"
+  },
+  {
+    "revision": "f6dba401e017c2821e27a21bc62808bb",
+    "url": "/test/static/media/email-2.f6dba401.svg"
+  },
+  {
+    "revision": "930a6325796ee92cb1356638c425541c",
+    "url": "/test/static/media/github.930a6325.svg"
+  },
+  {
+    "revision": "0f26720bbcd76cf8df11187be7dc0a44",
+    "url": "/test/static/media/gmail.0f26720b.svg"
+  },
+  {
+    "revision": "8b6c95d6af25024abd40bb045f16d307",
+    "url": "/test/static/media/instagram.8b6c95d6.svg"
+  },
+  {
+    "revision": "03c7255a5f023100a65e54b64d39e40c",
+    "url": "/test/static/media/linkedin.03c7255a.svg"
+  },
+  {
+    "revision": "3babbd64c47cc7afea454a41e97a12df",
+    "url": "/test/static/media/location.3babbd64.svg"
+  }
+]);
